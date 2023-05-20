897e664048a6afe393f6365b1921731c42db19c4
Danbi Choi <84640644+Danssi26@users.noreply.github.com>
2023-05-20 16:11:32 +0900

    [Bronze III] Title: 주사위 게임, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/10103. 주사위 게임/주사위 게임.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+A,B = 100,100
+for i in range(int(input())):
+    a,b = map(int,input().split())
+    if a > b:
+        B -= a
+    elif a < b:
+        A -= b
+print(A,B,sep="\n")
