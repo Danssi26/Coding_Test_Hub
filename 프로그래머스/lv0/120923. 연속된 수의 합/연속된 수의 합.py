90eddc00bf2fff8fc27f2a88a25218d329e35bd9
Danbi Choi <84640644+Danssi26@users.noreply.github.com>
2023-09-07 22:30:01 +0900

    Update 연속된 수의 합.py

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,9 +1,12 @@
 def solution(num, total):
     answer = []
-    # 연속된 수 num 개의 합은 total -> middle은 total을 num으로 나눈 몫
+    # 연속된 수 num 개의 합은 total -> middle(중간값)은 total을 num으로 나눈 몫
+    # middle 기준, 좌우로 값 조정하여 연속된 정수 생성
     middle = total // num
     
+    
     # num이 홀수일 때와 짝수일 때
+    # 중심값 기준, 좌우로 num/2 만큼 정수를 조합 -> 연속된 정수의 합 만들기
     # 짝수
     if num % 2 == 0: 
         start = middle - num // 2 + 1
@@ -11,7 +14,7 @@ def solution(num, total):
     else: 
         start = middle - num // 2
         
-    # 연속된 num 개의 정수를 구해서 answer 배열에 추가
+    # 연속된 수 num 개의 정수를 구해서 answer 배열에 추가
     for i in range(start, start + num):
         answer.append(i)
     return answer
