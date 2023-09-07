75aa72a9166d70d8cc7b482b96271f00368a5112
Danbi Choi <84640644+Danssi26@users.noreply.github.com>
2023-09-07 22:24:00 +0900

    [level 0] Title: 연속된 수의 합, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.py
+++ 프로그래머스/lv0/120923. 연속된 수의 합/연속된 수의 합.py
@@ -1,13 +1,13 @@
 def solution(num, total):
     answer = []
-    # 연속된 num 개의 정수의 합이 total이 되도록 가운데 값이 total을 num으로 나눈 몫으로
+    # 연속된 수 num 개의 합은 total -> middle은 total을 num으로 나눈 몫
     middle = total // num
     
-    # num이 홀수일 때와 짝수일 때를 나누어 처리
-    # 짝수일 경우
+    # num이 홀수일 때와 짝수일 때
+    # 짝수
     if num % 2 == 0: 
         start = middle - num // 2 + 1
-    # 홀수일 경우    
+    # 홀수
     else: 
         start = middle - num // 2
         
