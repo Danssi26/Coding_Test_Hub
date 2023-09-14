fb4d544ef43ad3e3261b079043fce5ed4a4ccc13
Danbi Choi <84640644+Danssi26@users.noreply.github.com>
2023-09-14 22:35:10 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
+++ 프로그래머스/lv0/120812. 최빈값 구하기/최빈값 구하기.py
@@ -1,23 +1,27 @@
 def solution(array):
-    # 배열에서 각 숫자 빈도 세기
-    # 빈 딕셔너리 생성, 숫자 빈도 저장
-    num_counts = {}
-    
-    # 배열 순회, 각 숫자 빈도 업데이트
-    for num in array:
-        if num in num_counts:
-            num_counts[num] += 1
+    array.sort()
+    max_count = 0
+    current_count = 1
+    mode = array[0]
+    multiple_mode = False
+
+    for i in range(1, len(array)):
+        if array[i] == array[i-1]:
+            current_count += 1
         else:
-            num_counts[num] = 1
+            if current_count > max_count:
+                max_count = current_count
+                mode = array[i-1]
+                multiple_mode = False
+            elif current_count == max_count:
+                multiple_mode = True
+
+            current_count = 1
+
+    if current_count > max_count:
+        mode = array[-1]
+        multiple_mode = False
+    elif current_count == max_count:
+        multiple_mode = True
     
-    # 빈도가 가장 높은 값
-    max_count = max(num_counts.values())
-    
-    # 빈도가 가장 높은 '값들'
-    answer = [num for num, count in num_counts.items() if count == max_count]
-    
-    # 최빈값이 여러 개면 -1 return
-    if len(answer) > 1:
-        return -1
-    else:
-        return answer[0]
+    return -1 if multiple_mode else mode
