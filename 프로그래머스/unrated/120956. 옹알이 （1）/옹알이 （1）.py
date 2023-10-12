dab6f5bb3f4c3f3f16b9c21ab6066dc930c57493
Danbi Choi <84640644+Danssi26@users.noreply.github.com>
2023-10-12 22:27:21 +0900

    [level unrated] Title: 옹알이 (1), Time: 0.03 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120956. 옹알이 （1）/옹알이 （1）.py
@@ -0,0 +1,20 @@
+def can_make_word(word, sounds):
+    if not word:  
+        return True
+    
+    for sound in sounds:
+        if word.startswith(sound): 
+            if can_make_word(word[len(sound):], [s for s in sounds if s != sound]):
+                return True
+            
+    return False  
+
+def solution(babbling):
+    possible_sounds = ["aya", "ye", "woo", "ma"]
+    answer = 0
+
+    for word in babbling:
+        if can_make_word(word, possible_sounds):  # Check if the word can be made using the sounds
+            answer += 1
+
+    return answer
